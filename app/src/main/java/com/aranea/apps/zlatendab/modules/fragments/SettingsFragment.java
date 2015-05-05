bcb7ab525cfd17cd054384464f2e778faed50438
olga <olahla@gmail.com>
2015-05-05 23:06:54 +0200

    change locale

--- app/src/main/java/com/aranea/apps/zlatendab/modules/fragments/SettingsFragment.java
+++ app/src/main/java/com/aranea/apps/zlatendab/modules/fragments/SettingsFragment.java
@@ -1,30 +1,42 @@
 package com.aranea.apps.zlatendab.modules.fragments;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 
 import com.aranea.apps.zlatendab.R;
 
+import java.util.Locale;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class SettingsFragment extends Fragment {
 
+    @InjectView(R.id.textViewGender)
+    TextView textViewLabelGender;
+    @InjectView(R.id.textViewLanguage)
+    TextView textViewLabelLanguage;
     @InjectView(R.id.radioButtonMale)
     RadioButton radioButtonMale;
     @InjectView(R.id.radioButtonFemale)
     RadioButton radioButtonFemale;
-    @InjectView(R.id.radioButtonEnglish)
-    RadioButton radioButtonEnglish;
-    @InjectView(R.id.radioButtonMacedonian)
-    RadioButton radioButtonMacedonian;
+    @InjectView(R.id.imageButtonFlagUk)
+    ImageButton imageButtonUk;
+    @InjectView(R.id.imageButtonFlagMk)
+    ImageButton imageButtonMk;
     @InjectView(R.id.editTextWeight)
     EditText editTextWeight;
 
@@ -39,6 +51,7 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.inject(this, view);
+        updateTextViews();
     }
 
 
@@ -46,5 +59,32 @@ public class SettingsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.reset(this);
+    }
+
+    @OnClick(R.id.imageButtonFlagUk)
+    public void changeLocaleUk() {
+        changeLocale("uk");
+    }
+
+    @OnClick(R.id.imageButtonFlagMk)
+    public void changeLocaleMk() {
+        changeLocale("mk");
+    }
+
+    private void changeLocale(String languageCode) {
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        Configuration configuration = getResources().getConfiguration();
+
+        configuration.locale = new Locale(languageCode.toLowerCase());
+        getResources().updateConfiguration(configuration, dm);
+        updateTextViews();
+    }
+
+    private void updateTextViews() {
+        radioButtonFemale.setText(getString(R.string.radio_button_female));
+        radioButtonMale.setText(getString(R.string.radio_button_male));
+        textViewLabelGender.setText(getString(R.string.label_gender));
+        editTextWeight.setHint(getString(R.string.edit_text_hint_weight));
+        textViewLabelLanguage.setText(getString(R.string.label_language));
     }
 }
